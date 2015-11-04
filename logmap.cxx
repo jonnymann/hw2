c3c504ff267683eced56e8da657f87c2f6d0e881
Johannes Stracke <jonny-stracke@web.de>
2015-11-04 22:21:57 +0100

    Hausaufgabe

--- logmap.cxx
+++ logmap.cxx
@@ -9,16 +9,17 @@ int main(){
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
 	double Data[100];
+        ofstream out("output.dat");
 	for(double r=0; r <= 4; r += 0.001){
 	   x=x0;
 	   for(int i=1; i <= Nskip; i++)
 		   x = r*x*(1-x);
 	   for(int i=Nskip+1; i <= Nend; i++){
 	   		   x = r*x*(1-x);
-	   		   Data[i-NSkip-1]=x	
+	   		   Data[i-Nskip-1]=x;	
    	   }
    	   for(int i=Nskip+1; i <= Nend; i++){
-	   	out << r << "\t" << Data[i-NSkip-1] << endl;
+	   	out << r << "\t" << Data[i-Nskip-1] << endl;
    	   }
    	    
 	}
